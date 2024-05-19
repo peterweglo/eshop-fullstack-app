6bcf22b7104106734e274843b7ccbea6cad1a177
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-05-19 17:18:47 +0200

    Add modal to ProductPage component

--- client/src/components/pages/ProductPage/ProductPage.js
+++ client/src/components/pages/ProductPage/ProductPage.js
@@ -6,11 +6,15 @@ import { Card, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { loadProductsRequest } from '../../../redux/productsRedux';
+import { Link } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
 
 const ProductPage = () => {
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const productData = useSelector((state) => getProductById(state, id));
 
@@ -64,10 +68,43 @@ const ProductPage = () => {
               <strong>Product updated :</strong> {productData.updatedAt}
             </Card.Text>
           </Card.Body>
-          <Button variant="outline-info" className="m-1">
-            Buy
+          <Button variant="outline-info" className="m-1" onClick={handleShow}>
+            Add to cart
           </Button>
         </Card>
+        <div>
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>The product has been added to the cart</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <Card>
+                <Card.Img
+                  variant="top"
+                  src="https://cdn.pixabay.com/photo/2013/07/12/18/20/shoes-153310_1280.png"
+                />
+                <Card.Body>
+                  <Card.Title>{productData.name}</Card.Title>
+                  <Card.Text>{productData.description}</Card.Text>
+                  <Card.Text>Price: {productData.price}$</Card.Text>
+                </Card.Body>
+              </Card>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Continue shopping
+              </Button>
+              <Button variant="primary">Go to cart</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
       </div>
     </main>
   );
