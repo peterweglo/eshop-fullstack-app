1eec784011d9d22bcc96a121cb2d49ed45fc46be
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-05-25 23:13:01 +0200

    Add build

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ProductsModule, OrdersModule],
+  imports: [
+    ProductsModule,
+    OrdersModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'client', 'build'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
